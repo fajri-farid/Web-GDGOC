099bc551c67fc63f00361ebf99289e2b720aedb2
fajri-farid <fajrijry14@gmail.com>
2024-11-09 22:54:49 +0800

    style: add responsive for something

--- script/navbar.js
+++ script/navbar.js
@@ -6,3 +6,22 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
+
+const navbar = document.querySelector("nav");
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+// Hamburger menu toggle
+hamburger.addEventListener("click", function () {
+  this.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+});
+
+// Navbar scroll effect
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 0) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
