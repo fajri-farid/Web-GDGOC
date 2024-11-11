3fab2f391e5de871b4df7070bff9cca93edb25bf
BryanJericho <bryanpanggalo@gmail.com>
2024-11-11 21:30:02 +0800

    feat: adding some changes and adding testimoni

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__, static_folder='../frontend', template_folder='templates')
 def index():
     return render_template('index.html')
 
+@app.route('/flag')
+def flag():
+    return "ya kali ada flag di website gdgoc -demon1"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
